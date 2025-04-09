37e3bd57d09e992def8f7780e4b0f6aadc611326
toi-la-qanh <quocanh2003vn427@gmail.com>
2025-04-09 22:14:29 +0700

    fix cookies issue

--- expressjs/src/controllers/user.controller.js
+++ expressjs/src/controllers/user.controller.js
@@ -74,9 +74,9 @@ class UserController {
       res.cookie("token", token, {
         path: "/", // Cookie is accessible from all paths
         expires: new Date(Date.now() + 86400000), // Cookie expires in 1 day
-        secure: false, // Cookie will only be sent over HTTPS
+        secure: process.env.NODE_ENV === "production", // Cookie will only be sent over HTTPS
         httpOnly: true, // Cookie cannot be accessed via client-side scripts
-        sameSite: "Lax",
+        sameSite: "None",
       });
 
       return res
